60b1e5ffef3f759a90075736f3d9f606a6d70715
Igor <ig.plotnikov@gmail.com>
2019-01-30 16:50:47 +0300

    user_creates_games spec

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -48,6 +48,7 @@ RSpec.configure do |config|
 
   config.include Devise::Test::ControllerHelpers, type: :controller
   config.include Devise::Test::ControllerHelpers, type: :view
+  config.include Warden::Test::Helpers, type: :feature
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -68,4 +69,6 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  #
+  Capybara.asset_host = 'http://localhost:3000'
 end
