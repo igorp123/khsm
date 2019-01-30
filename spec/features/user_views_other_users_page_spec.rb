4f1b9be716f0d970cb5fe1c219dd9307a42e925a
Igor <ig.plotnikov@gmail.com>
2019-01-31 00:14:59 +0300

    refactor user_views_other_users_page_spec

--- spec/features/user_views_other_users_page_spec.rb
+++ spec/features/user_views_other_users_page_spec.rb
@@ -1,14 +1,17 @@
 require 'rails_helper'
 
-RSpec.feature 'USER looks an alien profile', type: :feature do
+RSpec.feature 'USER views an other users page', type: :feature do
 
-  let(:users) { [FactoryBot.create(:user), FactoryBot.create(:user, name: 'Vasya')] }
+  let!(:vasya) { FactoryBot.create(:user, name: 'Vasya') }
+
+  let(:petya) { FactoryBot.create(:user, name: 'Petya') }
+
 
   let!(:games) do
     [
-      FactoryBot.create(:game, user: users.last, id: 2,
+      FactoryBot.create(:game, user: petya, id: 2,
                         current_level: 2, created_at: Time.parse('2018.01.10'), prize: 10000),
-      FactoryBot.create(:game, user: users.last, id: 747,
+      FactoryBot.create(:game, user: petya, id: 747,
                         current_level: 5, created_at: Time.parse('2018.01.20, 22:00'),
                         finished_at: Time.parse('2018.01.20, 22:20'), is_failed: false,
                         prize: 100000)
@@ -16,16 +19,17 @@ RSpec.feature 'USER looks an alien profile', type: :feature do
   end
 
   before(:each) do
-    login_as users.first
+    login_as vasya
   end
 
   scenario 'successfully' do
     visit '/'
-    click_link 'Vasya'
+
+    click_link 'Petya'
 
     expect(page).to have_current_path '/users/2'
 
-    expect(page).to have_content 'Vasya'
+    expect(page).to have_content 'Petya'
 
     expect(page).to have_content 'деньги'
     expect(page).to have_content 'в процессе'
